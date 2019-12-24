ff8adf259c7dab677ff3a01dddfbcd9112ccb42e
Shuhei Yoshino <s.yoshino.1113@gmail.com>
2019-12-25 03:15:45 +0900

    product-listにカードを追加

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-list.component.scss']
 })
 export class ProductListComponent implements OnInit {
-
+  products: any = [1,2,3,4]
   constructor() { }
 
   ngOnInit() {
