ee6ea418281b5e60f3ddd425595ddbbcee9f6af9
Shuhei Yoshino <s.yoshino.1113@gmail.com>
2019-12-25 15:58:45 +0900

    テンプレートの実装

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './common/navbar/navbar.component';
-import { ProductListComponent } from './product-list/product-list.component';
-import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ProductModule } from './product/product.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    ProductListComponent,
-    ProductDetailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ProductModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/products.ts
@@ -0,0 +1,32 @@
+export const PRODUCTS = [
+  {
+    coverImage: './assets/img/phone-cover.jpg',
+    name: "iPhone XL",
+    price: 700,
+    description: "A large phone with one of the best screen",
+    heading1: "サンプルテキスト1",
+    heading2: "サンプルテキスト2",
+    heading3: "サンプルテキスト3",
+    headingtext1: "サンプルですサンプルですサンプルですサンプルですサンプルですサンプルですサンプルですサンプルです",
+    headingtext2: "Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.",
+    headingtext3: "サンプルですサンプルですサンプルですサンプルですサンプルですサンプルですサンプルですサンプルです",
+  },
+  {
+    coverImage: './assets/img/phone-cover.jpg',
+    name: "iPhone Mini",
+    price: 600,
+    description: "A great phone with one of the best camera",
+    heading1: "サンプルテキスト1",
+    heading2: "サンプルテキスト2",
+    heading3: "サンプルテキスト3"
+  },
+  {
+    coverImage: './assets/img/phone-cover.jpg',
+    name: "iPhone Standard",
+    price: 300,
+    description: "",
+    heading1: "サンプルテキスト1",
+    heading2: "サンプルテキスト2",
+    heading3: "サンプルテキスト3"
+  }
+]
